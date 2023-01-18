f02aaf6cc10177b02181dcaaf5602bc068017c93
yeonsu <kimys9295@naver.com>
2023-01-18 17:12:04 +0900

    4 commit

--- public/basic/client/src/components/Profile/Profile.jsx
+++ public/basic/client/src/components/Profile/Profile.jsx
@@ -41,10 +41,212 @@ function Profile() {
 
             </div>
         </div>
+        <div className='profile-contents'>
+            <div className='feed-list'>
+
+            <div className='feed'>
+            <div className='top'>
+            <div className='profile-image'></div>
+            <div className='profile-desc'>
+            <div className='nickname txt-bold'>연수짱</div>
+            <div className='timestamp'>
+                11시 11분,어제
+            </div>
+            </div>
+            </div>
+            <div className='contents'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi rem magni et voluptatem, quas eum consequuntur iste ipsam quam ad iure error blanditiis incidunt consectetur neque exercitationem possimus ducimus reprehenderit!
+            </div>
+            <div className='bottom'>
+             <div className='like'>
+                <div className='asset'>
+                    <img src='/assets/feed/like-dac.svg'></img>
+                </div>
+                <div className='count txt-bold'>
+                    25K
+                </div>
+             </div>
+             <div className='comment'>
+                <div className='asset'>
+                <img src='/assets/feed/comment.svg'></img>
+                </div>
+                <div className='count txt-bold'>2K</div>
+             </div>
+            </div>
+
+        </div>
+        <div className='feed'>
+            <div className='top'>
+            <div className='profile-image'></div>
+            <div className='profile-desc'>
+            <div className='nickname txt-bold'>연수짱</div>
+            <div className='timestamp'>
+                11시 11분,어제
+            </div>
+            </div>
+            </div>
+            <div className='contents'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi rem magni et voluptatem, quas eum consequuntur iste ipsam quam ad iure error blanditiis incidunt consectetur neque exercitationem possimus ducimus reprehenderit!
+            </div>
+            <div className='bottom'>
+             <div className='like'>
+                <div className='asset'>
+                    <img src='/assets/feed/like-dac.svg'></img>
+                </div>
+                <div className='count txt-bold'>
+                    25K
+                </div>
+             </div>
+             <div className='comment'>
+                <div className='asset'>
+                <img src='/assets/feed/comment.svg'></img>
+                </div>
+                <div className='count txt-bold'>2K</div>
+             </div>
+            </div>
+
+        </div>
+        <div className='feed'>
+            <div className='top'>
+            <div className='profile-image'></div>
+            <div className='profile-desc'>
+            <div className='nickname txt-bold'>연수짱</div>
+            <div className='timestamp'>
+                11시 11분,어제
+            </div>
+            </div>
+            </div>
+            <div className='contents'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi rem magni et voluptatem, quas eum consequuntur iste ipsam quam ad iure error blanditiis incidunt consectetur neque exercitationem possimus ducimus reprehenderit!
+            </div>
+            <div className='bottom'>
+             <div className='like'>
+                <div className='asset'>
+                    <img src='/assets/feed/like-dac.svg'></img>
+                </div>
+                <div className='count txt-bold'>
+                    25K
+                </div>
+             </div>
+             <div className='comment'>
+                <div className='asset'>
+                <img src='/assets/feed/comment.svg'></img>
+                </div>
+                <div className='count txt-bold'>2K</div>
+             </div>
+            </div>
+
+        </div>
+        <div className='feed'>
+            <div className='top'>
+            <div className='profile-image'></div>
+            <div className='profile-desc'>
+            <div className='nickname txt-bold'>연수짱</div>
+            <div className='timestamp'>
+                11시 11분,어제
+            </div>
+            </div>
+            </div>
+            <div className='contents'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi rem magni et voluptatem, quas eum consequuntur iste ipsam quam ad iure error blanditiis incidunt consectetur neque exercitationem possimus ducimus reprehenderit!
+            </div>
+            <div className='bottom'>
+             <div className='like'>
+                <div className='asset'>
+                    <img src='/assets/feed/like-dac.svg'></img>
+                </div>
+                <div className='count txt-bold'>
+                    25K
+                </div>
+             </div>
+             <div className='comment'>
+                <div className='asset'>
+                <img src='/assets/feed/comment.svg'></img>
+                </div>
+                <div className='count txt-bold'>2K</div>
+             </div>
+            </div>
+
+        </div>
+        
+
+            <div className='feed'>
+                <div className='top'>
+                    <div className='profile-image'></div>
+                    <div className='profile-desc'>
+                        <div className='nickname txt-bold'>연수짱</div>
+                        <div className='timestamp'>
+                            11시 11분,어제
+                        </div>
+                    </div>
+                </div>
+                <div className='contents'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi rem magni et voluptatem, quas eum consequuntur iste ipsam quam ad iure error blanditiis incidunt consectetur neque exercitationem possimus ducimus reprehenderit!
+                    <div className='image'></div>
+                </div>
+                <div className='bottom'>
+                    <div className='like'>
+                        <div className='asset'>
+                            <img src='/assets/feed/like-dac.svg'></img>
+                        </div>
+                        <div className='count txt-bold'>
+                            25K
+                        </div>
+                    </div>
+                    <div className='comment'>
+                        <div className='asset'>
+                            <img src='/assets/feed/comment.svg'></img>
+                        </div>
+                        <div className='count txt-bold'>2K</div>
+                    </div>
+                 </div>
+            </div>
+
+        </div>
+            
+            <div className='profile-info-desc'>
+                <div className='desc'>
+                    <div className='title txt-bold'>
+                        좋아요
+                    </div>
+                    <div className='count'>
+                        739,000
+                    </div>
+                </div>
+
+                <div className='desc'>
+                    <div className='title txt-bold'>
+                        팔로워
+                    </div>
+                    <div className='count'>
+                        2438
+                    </div>
+                </div>
+                <div className='desc'>
+                    <div className='title txt-bold'>
+                        포스트
+                    </div>
+                    <div className='count'>
+                        320
+                    </div>
+                </div>
+                <div className='desc'>
+                    <div className='title txt-bold'>
+                        친구
+                    </div>
+                    <div className='count'>
+                        739,000
+                    </div>
+                </div>
+                
+            </div>
+            </div>
+
+        </div>
 
         </div>
       
-    </div>
+    
   )
 }
 
